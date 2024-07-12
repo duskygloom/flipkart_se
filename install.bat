359a4b4c2f0945f2cddafb864318a0be7d8c9b60
duskygloom <sarkarharsh708@gmail.com>
2024-07-12 21:07:15 +0530

    Changes in installation scripts

--- install.bat
+++ install.bat
@@ -3,13 +3,12 @@
 set runscript=flipkart.bat
 
 echo Installing requirements...
-pip install -r requirements.txt
+%CD%\Scripts\pip install -r requirements.txt
 
 echo Writing script...
 echo @echo OFF> %runscript%
 echo set prevdir=%%CD%%>> %runscript%
 echo set appdir=%CD%>> %runscript%
-echo set pydir=Scripts\python>> %runscript%
 echo cd %%appdir%%\src>> %runscript%
-echo %%appdir%%\%%pydir%% cli.py %%*>> %runscript%
+echo %%appdir%%\Scripts\python cli.py %%*>> %runscript%
 echo cd %%prevdir%%>> %runscript%
--- install.sh
+++ install.sh
@@ -3,15 +3,14 @@
 curdir=`pwd`
 
 echo "Installing requirements..."
-pip3 install -r requirements.txt
+$curdir/bin/pip install -r requirements.txt
 
 echo "Writing script..."
 echo "#!/usr/bin/env bash
 prevdir=\`pwd\`
 appdir='$curdir'
-pydir='bin/python'
 cd \$appdir/src
-\$appdir/\$pydir cli.py \$@
+\$appdir/bin/python cli.py \$@
 cd \$prevdir" > ./flipkart
 
 chmod +x ./flipkart
